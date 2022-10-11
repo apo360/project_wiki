2ed3fef123d11c211b91ddbfb8a33952c24b11f2
apo360 <apo360.20@gmail.com>
2022-10-11 15:52:55 +0100

    aply dropdown

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -29,10 +29,12 @@
 
 
   <!-- bootstrap core css -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- fonts style -->
 
@@ -43,6 +45,8 @@
   <!-- font awesome style -->
 
   <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+  <script src="https://kit.fontawesome.com/998c60ef77.js" crossorigin="anonymous"></script>
 
   <!-- owl carousel -->
 
--- resources/views/layouts/menu_principal.blade.php
+++ resources/views/layouts/menu_principal.blade.php
@@ -1,4 +1,5 @@
 
+
 <header class="header_section">
 
       <div class="container-fluid">
@@ -6,99 +7,74 @@
         <nav class="navbar navbar-expand-lg custom_nav-container ">
 
           <a class="navbar-brand" href="/" :active="request()->routeIs('/')">
-
-             <div class = "">
-
-                    <img src="images/favicon.png" class = "container_logo">
-
-              </div>
-
+            <img src="images/favicon.png" class="container_logo rounded-pill">
           </a>
 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-
             <span class=""> </span>
-
           </button>
 
-
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+          <div class="collapse navbar-collapse align-items-center" id="navbarSupportedContent" >
 
             <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
 
               <ul class="navbar-nav">
-
                 <li class="nav-item active">
-
                   <a class="nav-link" href="principal" :active="request()->routeIs('principal')">Início </a>
+                </li>
 
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Academicos</a>
+                  <ul class="dropdown-menu">
+                    <li class="dropdown-item" href="#">Inscrição Universitária</li>
+                    <li class="dropdown-item" href="#">Livraria</li>
+                    <li class="dropdown-item" href="#">###</li>
+                  </ul>
                 </li>
 
                 <li class="nav-item">
-
-                  <a class="nav-link" href="academico" :active="request()->routeIs('academico')">Academicos</a>
-
-                 <!-- <ul>
-                    <li>Inscrição Universitária</li>
-                    <li>Livraria</li>
-                    <li></li>
-                  </ul> -->
-
-                </li>
-
-                <li class="nav-item">
-
                   <a class="nav-link" href="repositorio" :active = "request()->routeIs('repositorio')">{{ __('Repositório') }}</a>
-
                 </li>
 
                 <li class="nav-item">
-
                   <a class="nav-link" href="colaborador" :active="request()->routeIs('colaborador')">Colaboradores</a>
-
                 </li>
 
                 <li class="nav-item">
-
-                  <a class="nav-link" href="contact.html">{{__('Anúncios')}}</a>
-
+                  <a class="nav-link" href="anuncio">{{__('Anúncios')}}</a>
                 </li>
-
               </ul>
+              
+            </div>
 
-              <div class="quote_btn-container" style = "margin-left: 100px">
-
+            <div class="d-flex mx-auto flex-column flex-lg-row">
+              <div class="quote_btn-container">
                 @if (Route::has('login'))
-
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-
-                    @auth
-
-                        <a href="{{ url('/index') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-
-                    @else
-
-                        <a href="{{ route('login') }}" class="text-sm text-gray-400 dark:text-gray-200 underline header__nav__link--cta header__nav__link">
-                            Entrar <i class="fa fa-user" aria-hidden="true"></i>
-                        </a>
-
-                        @if (Route::has('register'))
-
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-400 dark:text-gray-200 underline header__nav__link--cta header__nav__link">
-                              Register <i class="fa fa-lock" aria-hidden="true"></i> 
-                            </a>
-
-                        @endif
-
-                    @endauth
-
-                </div>
-
+                  <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                      @auth
+                          <a href="{{ url('/index') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                      @else
+                          <a href="{{ route('login') }}" class="text-sm text-gray-400 dark:text-gray-200 underline header__nav__link--cta header__nav__link">
+                              Entrar <i class="fa fa-user" aria-hidden="true"></i>
+                          </a>
+                          @if (Route::has('register'))
+                              <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-400 dark:text-gray-200 underline header__nav__link--cta header__nav__link">
+                                Register <i class="fa fa-lock" aria-hidden="true"></i> 
+                              </a>
+                          @endif
+                      @endauth
+                  </div>
                @endif
-
               </div>
+            </div>
 
+            <div>
+              <input type="checkbox" class="checkbox" id="chk" />
+              <label class="label" for="chk">
+                <i class="fas fa-sun"></i>
+                <i class="fas fa-moon"></i>
+                <div class="ball"></div>
+              </label>
             </div>
 
           </div>
@@ -108,3 +84,11 @@
       </div>
 
     </header>
+
+    <script>
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
+</script>
